cc435098c4e943400d6606c91d338e1ff30e2517
S.Alireza <amoazeni75@gmail.com>
2019-01-05 02:06:47 +0330

    - convert input image to gray scale - smooth gray scale image with gaussian filter

--- P2_9423110.py
+++ P2_9423110.py
@@ -1,3 +1,4 @@
+import img as img
 import numpy as np
 import cv2
 
@@ -12,10 +13,13 @@ blueImg[:, :, 1] = 0
 blueImg[:, :, 2] = 0
 cv2.imshow('Blue channel section 2', blueImg)
 
+# 3 convert to gray scale
+img_gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+cv2.imshow('Gray Scale Image section 3', img_gray)
 
-
-
-
+# 4 smooth gray scale image with gaussian filter
+gaussian_img = cv2.GaussianBlur(img_gray, (5, 5), 0, 0)   # kernel size 5 * 5
+cv2.imshow('Gaussian filter section 4', gaussian_img)
 
 # exit
 cv2.waitKey(0)
