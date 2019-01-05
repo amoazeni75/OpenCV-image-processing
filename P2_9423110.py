ca7d068cd1a949b27c533512b3f10ea868069a56
S.Alireza <amoazeni75@gmail.com>
2019-01-05 11:33:06 +0330

    - rotation with 90 degree - resize into w/2, h

--- P2_9423110.py
+++ P2_9423110.py
@@ -1,6 +1,8 @@
-import img as img
-import numpy as np
 import cv2
+# AI_Project 2
+# S.Alireza Moazeni
+# 9423110   Amirkabir university of technology
+# Fall 2018
 
 
 # 1 Loads image as such including alpha channel and then show it again
@@ -20,6 +22,14 @@ cv2.imshow('Gray Scale Image section 3', img_gray)
 # 4 smooth gray scale image with gaussian filter
 gaussian_img = cv2.GaussianBlur(img_gray, (5, 5), 0, 0)   # kernel size 5 * 5
 cv2.imshow('Gaussian filter section 4', gaussian_img)
+
+# 5 rotate input image with 90 degree
+rotated = cv2.rotate(img1, cv2.ROTATE_90_COUNTERCLOCKWISE)
+cv2.imshow('Rotated image 90 degree section 5', rotated)
+
+# 6 resize image to (w/2 , h)
+resize = cv2.resize(img1, (0,0), fx=0.5, fy=1)
+cv2.imshow('resize image section 6', resize)
 
 # exit
 cv2.waitKey(0)
