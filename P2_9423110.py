760454a1a3423cf78480463d03a6adf2b75a6f0f
S.Alireza <amoazeni75@gmail.com>
2019-01-21 17:32:27 +0330

    - face recognition - sample frame

--- P2_9423110.py
+++ P2_9423110.py
@@ -6,6 +6,8 @@ import cv2
 
 
 # 1 Loads image as such including alpha channel and then show it again
+import numpy as np
+
 img1 = cv2.imread('test.jpg', -1)
 cv2.imshow('Normal Image section 1', img1)
 
@@ -29,7 +31,61 @@ cv2.imshow('Rotated image 90 degree section 5', rotated)
 
 # 6 resize image to (w/2 , h)
 resize = cv2.resize(img1, (0,0), fx=0.5, fy=1)
-cv2.imshow('resize image section 6', resize)
+cv2.imshow('Resize image section 6', resize)
+
+# 7 edge detection with canny function
+edge = cv2.Canny(img1, 100, 200)
+cv2.imshow('Edges in image section 7', edge)
+
+# 8 segmentation
+img_segment = cv2.imread('test.jpg')
+ret, thresh = cv2.threshold(img_gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+# noise removal
+kernel = np.ones((3,3),np.uint8)
+opening = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel, iterations = 2)
+# sure background area
+sure_bg = cv2.dilate(opening,kernel,iterations=3)
+# Finding sure foreground area
+dist_transform = cv2.distanceTransform(opening,cv2.DIST_L2,5)
+ret, sure_fg = cv2.threshold(dist_transform,0.7*dist_transform.max(),255,0)
+# Finding unknown region
+sure_fg = np.uint8(sure_fg)
+unknown = cv2.subtract(sure_bg,sure_fg)
+# Marker labelling
+ret, markers = cv2.connectedComponents(sure_fg)
+# Add one to all labels so that sure background is not 0, but 1
+markers = markers+1
+# Now, mark the region of unknown with zero
+markers[unknown==255] = 0
+markers = cv2.watershed(img_segment,markers)
+img_segment[markers == -1] = [255,0,0]
+cv2.imshow('Segmentation section 8', img_segment)
+
+
+#9 face detection
+face_cascade = cv2.CascadeClassifier('C:\\opencv\\build\\etc\\haarcascades\\haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('C:\\opencv\\build\\etc\\haarcascades\\haarcascade_eye.xml')
+
+img = cv2.imread('test.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+for (x,y,w,h) in faces:
+    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+    roi_gray = gray[y:y+h, x:x+w]
+    roi_color = img[y:y+h, x:x+w]
+
+cv2.imshow('Face Detection', img)
+
+#10 extract 5 frame
+vidcap = cv2.VideoCapture("test.avi")
+
+for i in range(4):
+    success, image = vidcap.read()
+    print(success)
+    cv2.imshow('capture ' + i, image)
+    # time.sleep(1)
+    cv2.waitKey(500)
 
 # exit
 cv2.waitKey(0)
