4ce2a6fd2e45d9825fb2983bdf043df9c8a55a99
S.Alireza <amoazeni75@gmail.com>
2019-01-21 18:21:56 +0330

    - some bug fixed 3

--- P2_9423110.py
+++ P2_9423110.py
@@ -64,8 +64,7 @@ cv2.imshow('Segmentation section 8', img_segment)
 
 
 #9 face detection
-face_cascade = cv2.CascadeClassifier('C:\\opencv\\build\\etc\\haarcascades\\haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('C:\\opencv\\build\\etc\\haarcascades\\haarcascade_eye.xml')
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 img = cv2.imread('test.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
